4ee2af8fbddad639a91d0fa5b28c3e9ca0ed6086
William Van Etten <bill@bioteam.net>
2013-06-25 09:22:09 -0400

    try a different dependent delete

--- lib/bio/io/biosql/ar-biosql.rb
+++ lib/bio/io/biosql/ar-biosql.rb
@@ -5,115 +5,101 @@ module Bio
       has_many :bioentries, :class_name =>"Bioentry", :foreign_key => "biodatabase_id", :dependent => :destroy
       validates_uniqueness_of :name
     end
+
     class BioentryDbxref < DummyBase
+      self.primary_key = nil
       attr_accessible :bioentry, :dbxref
-      set_primary_key nil #bioentry_id,dbxref_id
       belongs_to :bioentry, :class_name => "Bioentry"
       belongs_to :dbxref, :class_name => "Dbxref"
     end
 
     class BioentryPath < DummyBase
+      self.primary_key = nil
       attr_accessible :distance, :term, :object_bioentry, :subject_bioentry
-      set_primary_key nil
-      #delete				set_sequence_name nil
       belongs_to :term, :class_name => "Term"
-      #da sistemare per poter procedere.
       belongs_to :object_bioentry, :class_name=>"Bioentry"
       belongs_to :subject_bioentry, :class_name=>"Bioentry"
-    end #BioentryPath
+    end
 
     class BioentryQualifierValue < DummyBase
-      #NOTE: added rank to primary_keys, now it's finished.
       attr_accessible :value, :rank, :bioentry, :term
-      set_primary_keys :bioentry_id, :term_id, :rank
+      self.primary_keys = :bioentry_id, :term_id, :rank
       belongs_to :bioentry, :class_name => "Bioentry"
       belongs_to :term, :class_name => "Term"
-    end #BioentryQualifierValue
+    end
   
     class Bioentry < DummyBase
       attr_accessible :name, :accession, :identifier, :division, :description, :version, :biodatabase, :taxon
       belongs_to :biodatabase, :class_name => "Biodatabase"
       belongs_to :taxon, :class_name => "Taxon"
-      has_one :biosequence, :dependent => :destroy
-      #, :class_name => "Biosequence", :foreign_key => "bioentry_id"
+      has_one :biosequence, :class_name => "Biosequence", :dependent => :destroy
       has_many :comments, :class_name =>"Comment", :order =>'rank', :dependent => :destroy
       has_many :seqfeatures, :class_name => "Seqfeature", :order=>'rank', :dependent => :destroy
-# no delete
-      has_many :bioentry_references, :class_name=>"BioentryReference", :dependent => :destroy #, :foreign_key => "bioentry_id"
       has_many :bioentry_dbxrefs, :class_name => "BioentryDbxref", :dependent => :destroy
       has_many :object_bioentry_relationships, :class_name=>"BioentryRelationship", :foreign_key=>"object_bioentry_id", :dependent => :destroy #non mi convince molto credo non funzioni nel modo corretto
       has_many :subject_bioentry_relationships, :class_name=>"BioentryRelationship", :foreign_key=>"subject_bioentry_id", :dependent => :destroy #non mi convince molto credo non funzioni nel modo corretto
       has_many :object_bioentry_paths, :class_name=>"BioentryPath", :foreign_key=>"object_bioentry_id", :dependent => :destroy #non mi convince molto credo non funzioni nel modo corretto
       has_many :subject_bioentry_paths, :class_name=>"BioentryPath", :foreign_key=>"subject_bioentry_id", :dependent => :destroy #non mi convince molto credo non funzioni nel modo corretto
-# no delete
       has_many :cdsfeatures, :class_name=>"Seqfeature", :foreign_key =>"bioentry_id", :conditions=>["term.name='CDS'"], :include=>"type_term"
+# no delete
+      has_many :bioentry_references, :class_name=>"BioentryReference", :dependent => :destroy
       has_many :references, :through=>:bioentry_references, :class_name => "Reference", :dependent => :destroy
 # no delete
-      has_many :terms, :through=>:bioentry_qualifier_values, :class_name => "Term", :dependent => :destroy
-      #NOTE: added order_by for multiple hit and manage ranks correctly
       has_many :bioentry_qualifier_values, :order=>"bioentry_id,term_id,rank", :class_name => "BioentryQualifierValue", :dependent => :destroy
-        
-      #per la creazione richiesti:
-      #name, accession, version
-      #				validates_uniqueness_of :accession, :scope=>[:biodatabase_id]
-      #				validates_uniqueness_of :name, :scope=>[:biodatabase_id]
-			#	validates_uniqueness_of :identifier, :scope=>[:biodatabase_id]
-				
+      has_many :terms, :through=>:bioentry_qualifier_values, :class_name => "Term", :dependent => :destroy
     end
+
     class BioentryReference < DummyBase
       attr_accessible :start_pos, :end_pos, :rank, :bioentry, :reference
-      set_primary_keys :bioentry_id, :reference_id, :rank
+      self.primary_keys = :bioentry_id, :reference_id, :rank
       belongs_to :bioentry, :class_name => "Bioentry"
       belongs_to :reference , :class_name => "Reference"
     end
+
     class BioentryRelationship < DummyBase
       attr_accessible :rank, :term, :subject_bioentry, :object_bioentry
-      #delete				set_primary_key "bioentry_relationship_id"
       set_sequence_name "bieontry_relationship_pk_seq"
       belongs_to :object_bioentry, :class_name => "Bioentry"
       belongs_to :subject_bioentry, :class_name => "Bioentry"
-      belongs_to :term
+      belongs_to :term, :class_name => "Term"
     end
+
     class Biosequence < DummyBase
       attr_accessible :version, :length, :alphabet, :seq
-      set_primary_keys :bioentry_id, :version
-      #delete				set_sequence_name "biosequence_pk_seq"
-      belongs_to :bioentry, :foreign_key=>"bioentry_id"
-      #has_one :bioentry
-      #, :class_name => "Bioentry"
+      self.primary_keys = :bioentry_id, :version
+      belongs_to :bioentry, :class_name => "Bioentry", :foreign_key=>"bioentry_id"
     end
+
     class Comment < DummyBase
       attr_accessible :comment_text, :rank, :bioentry
       belongs_to :bioentry, :class_name => "Bioentry"
     end
+
     class DbxrefQualifierValue < DummyBase
+      self.primary_key = nil
       attr_accessible :rank, :value, :term, :dbxref
-      #think to use composite primary key
-      set_primary_key nil #dbxref_id, term_id, rank
-      #delete			      set_sequence_name nil
       belongs_to :dbxref, :class_name => "Dbxref"
       belongs_to :term, :class_name => "Term"
     end
+
     class Dbxref < DummyBase
       attr_accessible :dbname, :accession, :version
-      #set_sequence_name "dbxref_pk_seq"
       has_many :dbxref_qualifier_values, :class_name => "DbxrefQualifierValue", :dependent => :destroy
       has_many :locations, :class_name => "Location", :dependent => :destroy
       has_many :references, :class_name=>"Reference", :dependent => :destroy
       has_many :term_dbxrefs, :class_name => "TermDbxref", :dependent => :destroy
       has_many :bioentry_dbxrefs, :class_name => "BioentryDbxref", :dependent => :destroy
-      #TODO: check is with bioentry there is an has_and_belongs_to_many relationship has specified in schema overview.
     end
+
     class LocationQualifierValue <  DummyBase
+      self.primary_key = nil
       attr_accessible :value, :int_value, :location, :term
-      set_primary_key nil #location_id, term_id
-      #delete			      set_sequence_name nil
       belongs_to :location, :class_name => "Location"
       belongs_to :term, :class_name => "Term"
     end
+
     class Location < DummyBase
       attr_accessible :start_pos, :end_pos, :strand, :rank, :seqfeature, :dbxref, :term
-      #set_sequence_name "location_pk_seq"
       belongs_to :seqfeature, :class_name => "Seqfeature"
       belongs_to :dbxref, :class_name => "Dbxref"
       belongs_to :term, :class_name => "Term"
@@ -136,55 +122,55 @@ module Bio
         end
         return seq        
       end
-      
-      
-      
     end
+
     class Ontology < DummyBase
       attr_accessible :name, :definition
       has_many :terms, :class_name => "Term", :dependent => :destroy
       has_many :term_paths, :class_name => "TermPath", :dependent => :destroy
       has_many :term_relationships, :class_name => "TermRelationship", :dependent => :destroy
     end
+
     class Reference < DummyBase
       attr_accessible :location, :title, :authors, :crc, :dbxref
       belongs_to :dbxref, :class_name => "Dbxref"
       has_many :bioentry_references, :class_name=>"BioentryReference", :dependent => :destroy
 # no delete
-      has_many :bioentries, :through=>:bioentry_references
+      has_many :bioentries, :through=>:bioentry_references, :dependent => :destroy
     end
+
     class SeqfeatureDbxref < DummyBase
       attr_accessible :rank, :seqfeature, :dbxref
-      set_primary_keys :seqfeature_id, :dbxref_id
-      #delete		      set_sequence_name nil
+      self.primary_keys = :seqfeature_id, :dbxref_id
       belongs_to :seqfeature, :class_name => "Seqfeature", :foreign_key => "seqfeature_id"
       belongs_to :dbxref, :class_name => "Dbxref", :foreign_key => "dbxref_id"
     end
+
     class SeqfeaturePath < DummyBase
       attr_accessible :distance, :term, :subject_seqfeature, :object_seqfeature
-      set_primary_keys :object_seqfeature_id, :subject_seqfeature_id, :term_id
+      self.primary_keys = :object_seqfeature_id, :subject_seqfeature_id, :term_id
       set_sequence_name nil
       belongs_to :object_seqfeature, :class_name => "Seqfeature", :foreign_key => "object_seqfeature_id"
       belongs_to :subject_seqfeature, :class_name => "Seqfeature", :foreign_key => "subject_seqfeature_id"
       belongs_to :term, :class_name => "Term"
     end
+
     class SeqfeatureQualifierValue < DummyBase
       attr_accessible :rank, :value, :term, :seqfeature
-      set_primary_keys  :seqfeature_id, :term_id, :rank
+      self.primary_keys =  :seqfeature_id, :term_id, :rank
       set_sequence_name nil
       belongs_to :seqfeature
       belongs_to :term, :class_name => "Term"
     end		
+
     class Seqfeature <DummyBase  
       attr_accessible :display_name, :rank, :source_term, :type_term, :bioentry
       set_sequence_name "seqfeature_pk_seq"
-      belongs_to :bioentry
-      #, :class_name => "Bioentry"
+      belongs_to :bioentry, :class_name => "Bioentry"
       belongs_to :type_term, :class_name => "Term", :foreign_key => "type_term_id"
       belongs_to :source_term, :class_name => "Term", :foreign_key =>"source_term_id"
       has_many :seqfeature_dbxrefs, :class_name => "SeqfeatureDbxref", :foreign_key => "seqfeature_id", :dependent => :destroy
-      has_many :seqfeature_qualifier_values, :order=>'rank', :foreign_key => "seqfeature_id", :dependent => :destroy
-      #, :class_name => "SeqfeatureQualifierValue"
+      has_many :seqfeature_qualifier_values, :class_name => "SeqfeatureQualifierValue", :order=>'rank', :foreign_key => "seqfeature_id", :dependent => :destroy
       has_many :locations, :class_name => "Location", :order=>'rank', :dependent => :destroy
       has_many :object_seqfeature_paths, :class_name => "SeqfeaturePath", :foreign_key => "object_seqfeature_id", :dependent => :destroy
       has_many :subject_seqfeature_paths, :class_name => "SeqfeaturePath", :foreign_key => "subject_seqfeature_id", :dependent => :destroy
@@ -203,6 +189,7 @@ module Bio
         self.sequence.translate(*args)
       end
     end
+
     class SeqfeatureRelationship <DummyBase
       attr_accessible :rank, :term, :object_seqfeature, :subject_seqfeature
       set_sequence_name "seqfeatue_relationship_pk_seq"
@@ -210,11 +197,13 @@ module Bio
       belongs_to :object_seqfeature, :class_name => "Seqfeature"
       belongs_to :subject_seqfeature, :class_name => "Seqfeature"
     end
+
     class TaxonName < DummyBase
       attr_accessible :name, :name_class, :taxon
-      set_primary_keys :taxon_id, :name, :name_class
+      self.primary_keys = :taxon_id, :name, :name_class
       belongs_to :taxon, :class_name => "Taxon"
     end
+
     class Taxon < DummyBase
       attr_accessible :ncbi_taxon_id, :parent_taxon_id, :node_rank, :genetic_code, :mito_genetic_code, :left_value, :right_value
       set_sequence_name "taxon_pk_seq"
@@ -225,13 +214,14 @@ module Bio
       has_one :taxon_genbank_common_name, :class_name => "TaxonName", :conditions=>"name_class = 'genbank common name'"
       has_one :bioentry, :class_name => "Bioentry", :dependent => :destroy
     end
+
     class TermDbxref < DummyBase
+      self.primary_key = nil
       attr_accessible :rank, :term, :dbxref
-      set_primary_key nil #term_id, dbxref_id
-      #delete			      set_sequence_name nil
       belongs_to :term, :class_name => "Term"
       belongs_to :dbxref, :class_name => "Dbxref"
     end
+
     class TermPath < DummyBase
       attr_accessible :distance, :ontology, :subject_term, :object_term, :predicate_term
       set_sequence_name "term_path_pk_seq"
@@ -240,6 +230,7 @@ module Bio
       belongs_to :object_term, :class_name => "Term"
       belongs_to :predicate_term, :class_name => "Term"
     end
+
     class Term < DummyBase
       attr_accessible :name, :definition, :identifier, :is_obsolete, :ontology
       belongs_to :ontology, :class_name => "Ontology"
@@ -247,7 +238,7 @@ module Bio
       has_many :dbxref_qualifier_values, :class_name => "DbxrefQualifierValue", :dependent => :destroy
       has_many :bioentry_qualifer_values, :class_name => "BioentryQualifierValue", :dependent => :destroy
 # no delete
-      has_many :bioentries, :through=>:bioentry_qualifier_values
+      has_many :bioentries, :through=>:bioentry_qualifier_values, :dependent => :destroy
       has_many :locations, :class_name => "Location", :dependent => :destroy
       has_many :seqfeature_relationships, :class_name => "SeqfeatureRelationship", :dependent => :destroy
       has_many :term_dbxrefs, :class_name => "TermDbxref", :dependent => :destroy
@@ -264,6 +255,7 @@ module Bio
       has_many :term_relationship_objects, :class_name => "TermRelationship", :foreign_key =>"object_term_id", :dependent => :destroy
       has_many :seqfeature_paths, :class_name => "SeqfeaturePath", :dependent => :destroy
     end
+
     class TermRelationship < DummyBase
       attr_accessible :ontology, :subject_term, :predicate_term, :object_term
       set_sequence_name "term_relationship_pk_seq"
@@ -273,18 +265,19 @@ module Bio
       belongs_to :object_term, :class_name => "Term"
       has_one :term_relationship_term, :class_name => "TermRelationshipTerm", :dependent => :destroy
     end
+
     class TermRelationshipTerm < DummyBase
       attr_accessible :term_relationship, :term
-      #delete			      set_sequence_name nil
-      set_primary_key :term_relationship_id
+      self.primary_key = 'term_relationship_id'
       belongs_to :term_relationship, :class_name => "TermRelationship"
       belongs_to :term, :class_name => "Term"
     end
+
     class TermSynonym < DummyBase
-      #delete			      set_sequence_name nil
+      self.primary_key = nil
       attr_accessible :synonym, :term
-      set_primary_key nil
       belongs_to :term, :class_name => "Term"
     end
+
   end #SQL
 end #Bio
